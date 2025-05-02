bd30bb97aacce4d8d310cde81080e0eb60ac5ed9
151rml <lal20001@byui.edu>
2025-05-03 07:49:26 +1200

    2

--- week01/activity02.js
+++ week01/activity02.js
@@ -2,6 +2,6 @@ const myApples = 4;
 const friendApples = 2;
 let total = myApples + friendApples;
 
-document.getElementById("myAppleElement").textContent = myApples;
+document.getElementById("myApplesElement").textContent = myApples;
 document.getElementById("friendApplesElement").textContent = friendApples;
 document.getElementById("totalApplesElement").textContent = total;
